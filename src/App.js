660cf4eeea0ec7c9cdbc476c7a635ca9d6ef46db
SGDS666 <1226931269@qq.com>
2022-01-09 08:17:00 +0800

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.less';
 
 export default function App() {
-  return <div>app</div>;
+  const gamearr = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  const [game, setgame] = useState(gamearr);
+  return (
+    <div id="app">
+      <div id="game">
+        {game.map((item) => {
+          return <div>{item}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
